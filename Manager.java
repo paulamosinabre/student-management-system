3dd1fc14595fc9e6d10bf79044c02da4b0902d0f
Paula Bianca Mosinabre <mosinabre.paulabianca@gmail.com>
2025-10-13 00:42:56 +0800

    Update Manager.java

--- Manager.java
+++ Manager.java
@@ -16,54 +16,15 @@ public class Manager {
 
     public void delete(int index) {
         if (index - 1 >= 0 && index - 1 < students.size()) {
-            students.remove(index);
+            students.remove(index-1);
         } else {
             System.out.println("Invalid number!");
         }
     }
-    //Solution 1
-/*
-    public void updateName(String name, int index) {
-        if (index - 1 >= 0 && index - 1 < students.size()) {
-            System.out.println("The name has been updated from " + this.students.get(index - 1).getName() + " to" + name);
-            this.students.get(index - 1).setName(name);
-        } else {
-            System.out.println("Invalid number!");
-        }
-    }
-
-    public void updateGender(String gender, int index) {
-        if (index - 1 >= 0 && index - 1 < students.size()) {
-            System.out.println("The gender has been updated from " + this.students.get(index - 1).getGender() + " to" + gender);
-            this.students.get(index - 1).setGender(gender);
-        } else {
-            System.out.println("Invalid number!");
-        }
-    }
-
-    public void updateCourse(String course, int index) {
-        if (index - 1 >= 0 && index - 1 < students.size()) {
-            System.out.println("The course has been updated from " + this.students.get(index - 1).getCourse() + " to" + course);
-            this.students.get(index - 1).setCourse(course);
-        } else {
-            System.out.println("Invalid number!");
-        }
-    }
-
-    public void updateYear(String year, int index) {
-        if (index - 1 >= 0 && index - 1 < students.size()) {
-            System.out.println("The year has been updated from " + this.students.get(index - 1).getYear() + " to" + year);
-            this.students.get(index - 1).setYear(year);
-        } else {
-            System.out.println("Invalid number!");
-        }
-    }
-     */
-
-    //Solution 2
+    
     public void updateGPA(double GPA, int index) {
         if (index - 1 >= 0 && index - 1 < students.size()) {
-            System.out.println("The GPA has been updated from " + this.students.get(index - 1).getGPA() + " to" + GPA);
+            System.out.println("The GPA has been updated from " + this.students.get(index - 1).getGPA() + " to " + GPA);
             this.students.get(index - 1).setGPA(GPA);
         } else {
             System.out.println("Invalid number!");
@@ -74,19 +35,19 @@ public class Manager {
         if (index - 1 >= 0 && index - 1 < students.size()) {
             switch (classification) {
                 case 1:
-                    System.out.println("The name has been updated from " + this.students.get(index - 1).getName() + " to" + update);
+                    System.out.println("The name has been updated from " + this.students.get(index - 1).getName() + " to " + update);
                     this.students.get(index - 1).setName(update);
                     break;
                 case 2:
-                    System.out.println("The name has been updated from " + this.students.get(index - 1).getGender() + " to" + update);
+                    System.out.println("The name has been updated from " + this.students.get(index - 1).getGender() + " to " + update);
                     this.students.get(index - 1).setGender(update);
                     break;
                 case 3:
-                    System.out.println("The name has been updated from " + this.students.get(index - 1).getCourse() + " to" + update);
+                    System.out.println("The name has been updated from " + this.students.get(index - 1).getCourse() + " to " + update);
                     this.students.get(index - 1).setCourse(update);
                     break;
                 case 4:
-                    System.out.println("The name has been updated from " + this.students.get(index - 1).getYear() + " to" + update);
+                    System.out.println("The name has been updated from " + this.students.get(index - 1).getYear() + " to " + update);
                     this.students.get(index - 1).setYear(update);
                     break;
                 default:
@@ -97,6 +58,7 @@ public class Manager {
         }
     }
     
+    
     public void sort(){
         
     }
@@ -105,7 +67,7 @@ public class Manager {
         
     }
     public void showDetails() {
-        System.out.println("================================================================================================================");
+        System.out.println("\n================================================================================================================");
         System.out.printf("%-4s | %-12s | %-20s | %-8s | %-10s | %-10s | %-4s%n",
                 "No.", "Student ID", "Name", "Gender", "Course", "Year", "GPA");
         System.out.println("================================================================================================================");
