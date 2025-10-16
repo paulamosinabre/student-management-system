e28819e4e37257e5437af6219f27d44bec263c4a
Paula Bianca Mosinabre <mosinabre.paulabianca@gmail.com>
2025-10-16 13:48:55 +0800

    Update Manager.java

--- Manager.java
+++ Manager.java
@@ -11,20 +11,75 @@ public class Manager {
     }
 
     public void add(Student student) {
-        //gumawa ng if student alrd exists di na ma-aaadd
-        students.add(student);
+        if (student.getGPA() >= 0 && student.getGPA() <= 4.0) {
+            students.add(student);
+            System.out.println("Successfully Added!");
+        } else {
+            System.out.println("Could not add student. Make sure the GPA does not exceed 4.0");
+        }
+
     }
 
     public void delete(int index) {
-        if (index - 1 >= 0 && index - 1 < students.size()) {
+        if (isItInTheList(index)) {
             students.remove(index - 1);
+            System.out.println("Successfully Deleted!");
+        } else {
+            System.out.println("Invalid number!");
+        }
+    }
+
+    public boolean isItInTheList(int index) {
+        return index - 1 >= 0 && index - 1 < students.size();
+    }
+
+    public void updateFirstName(String firstName, int index) {
+        if (isItInTheList(index)) {
+            this.students.get(index - 1).setFirstName(firstName);
+            System.out.println("Successfully Updated!");
+        } else {
+            System.out.println("Invalid number!");
+        }
+    }
+
+    public void updateLastName(String lastName, int index) {
+        if (isItInTheList(index)) {
+            this.students.get(index - 1).setLastName(lastName);
+            System.out.println("Successfully Updated!");
+        } else {
+            System.out.println("Invalid number!");
+        }
+    }
+
+    public void updateGender(String gender, int index) {
+        if (isItInTheList(index)) {
+            this.students.get(index - 1).setGender(gender);
+            System.out.println("Successfully Updated!");
+        } else {
+            System.out.println("Invalid number!");
+        }
+    }
+
+    public void updateCourse(String course, int index) {
+        if (isItInTheList(index)) {
+            this.students.get(index - 1).setCourse(course);
+            System.out.println("Successfully Updated!");
+        } else {
+            System.out.println("Invalid number!");
+        }
+    }
+
+    public void updateYear(String year, int index) {
+        if (isItInTheList(index)) {
+            this.students.get(index - 1).setYear(year);
+            System.out.println("Successfully Updated!");
         } else {
             System.out.println("Invalid number!");
         }
     }
 
     public void updateGPA(double GPA, int index) {
-        if (index - 1 >= 0 && index - 1 < students.size()) {
+        if (isItInTheList(index) && (this.students.get(index - 1).getGPA() >= 0 && this.students.get(index - 1).getGPA() <= 4.0)) {
             System.out.println("The GPA has been updated from " + this.students.get(index - 1).getGPA() + " to " + GPA);
             this.students.get(index - 1).setGPA(GPA);
         } else {
@@ -32,39 +87,47 @@ public class Manager {
         }
     }
 
-    public void update(String update, int index, int classification) {
-        if (index - 1 >= 0 && index - 1 < students.size()) {
-            switch (classification) {
-                case 1:
-                    System.out.println("The name has been updated from " + this.students.get(index - 1).getName() + " to " + update);
-                    this.students.get(index - 1).setName(update);
-                    break;
-                case 2:
-                    System.out.println("The name has been updated from " + this.students.get(index - 1).getGender() + " to " + update);
-                    this.students.get(index - 1).setGender(update);
-                    break;
-                case 3:
-                    System.out.println("The name has been updated from " + this.students.get(index - 1).getCourse() + " to " + update);
-                    this.students.get(index - 1).setCourse(update);
-                    break;
-                case 4:
-                    System.out.println("The name has been updated from " + this.students.get(index - 1).getYear() + " to " + update);
-                    this.students.get(index - 1).setYear(update);
-                    break;
-                default:
-                    System.out.println("Invalid number!");
-            }
-        } else {
-            System.out.println("Invalid number!");
+    public boolean isNumeric(String search) {
+        if (search == null || search.isEmpty()) {
+            return false;
+        }
+
+        try {
+            Double.parseDouble(search);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
         }
     }
 
     public void search(String search) {
         boolean found = false;
-        for (Student student : students) {
-            if (student.getId().equals(search) || student.getName().equals(search) || student.getCourse().equals(search) || student.getGPA() == Integer.parseInt(search)) {
+
+        for (Student student: students) {
+            boolean match
+                    = student.getId().equalsIgnoreCase(search)
+                    || student.getFirstName().equalsIgnoreCase(search)
+                    || student.getLastName().equalsIgnoreCase(search)
+                    || student.getFullName().equalsIgnoreCase(search)
+                    || student.getCourse().equalsIgnoreCase(search);
+
+            // If input is numeric, compare with GPA
+            if (isNumeric(search)) {
+                double gpaSearch = Double.parseDouble(search);
+                if (student.getGPA() == gpaSearch) {
+                    match = true;
+                }
+            }
+
+            if (match) {
                 found = true;
-                showDetails();
+
+                System.out.println("\n============================================= Search Results ===============================================");
+                System.out.printf("%-12s | %-26s | %-8s | %-10s | %-10s | %-4s%n",
+                         "Student ID", "Name", "Gender", "Course", "Year", "GPA");
+                System.out.println("============================================================================================================");
+
+                System.out.println(student);
             }
         }
 
@@ -76,35 +139,44 @@ public class Manager {
     public void sortBy(int type) {
         switch (type) {
             case 1:
-                students.sort((s1, s2) -> s1.getName().compareTo(s2.getName()));
+                //lambda comparator
+                students.sort((s1, s2) -> s1.getFullName().compareTo(s2.getFullName()));
                 showDetails();
                 break;
-                
             case 2:
+                students.sort((s1, s2) -> s2.getFullName().compareTo(s1.getFullName()));
+                showDetails();
                 break;
-                
             case 3:
                 students.sort((s1, s2) -> Double.compare(s1.getGPA(), s2.getGPA()));
                 showDetails();
                 break;
-                
             case 4:
+                students.sort((s1, s2) -> Double.compare(s2.getGPA(), s1.getGPA()));
+                showDetails();
                 break;
-            
             case 5:
                 students.sort((s1, s2) -> s1.getCourse().compareTo(s2.getCourse()));
+                showDetails();
                 break;
-                
+
         }
     }
-    
-    public void topStudents(){
-        
+
+    public void topStudents() {
+        students.sort((s1, s2) -> Double.compare(s2.getGPA(), s1.getGPA()));
+        System.out.println("\n=============================================== Top Students ===================================================");
+        System.out.printf("%-4s | %-12s | %-26s | %-8s | %-10s | %-10s | %-4s%n",
+                "No.", "Student ID", "Name", "Gender", "Course", "Year", "GPA");
+        System.out.println("================================================================================================================");
+        for (int i = 0; i < 3; i++) {
+            System.out.printf("%-4d | %s%n", (i + 1), students.get(i).toString());
+        }
     }
 
     public void showDetails() {
         System.out.println("\n================================================================================================================");
-        System.out.printf("%-4s | %-12s | %-20s | %-8s | %-10s | %-10s | %-4s%n",
+        System.out.printf("%-4s | %-12s | %-26s | %-8s | %-10s | %-10s | %-4s%n",
                 "No.", "Student ID", "Name", "Gender", "Course", "Year", "GPA");
         System.out.println("================================================================================================================");
 
